fa2e3805c5f1931d67fcca8442de08e522cf231e
David <davidmartin71090@gmail.com>
2022-09-17 12:49:21 -0400

    Add userTimer hook

---
+++ hooks/useTimer.ts
@@ -0,0 +1,33 @@
+import { useState, useCallback, useEffect } from "react"
+
+type TimerStages = "not started" | "running" | "complete"
+
+const useTimer = (startingTime = 60) => {
+  const [timer, setTimer] = useState(startingTime)
+  const [stage, setStage] = useState("not started")
+
+  const start = useCallback(() => {
+    const interval = setInterval(() => {
+      setTimer(timer => {
+        setStage("running")
+        let updatedTimer = (timer -= 1)
+
+        if (updatedTimer <= 0) {
+          setStage("complete")
+          clearInterval(interval)
+          return startingTime
+        }
+        return updatedTimer
+      })
+    }, 1000)
+  }, [setTimer, startingTime])
+
+  const reset = useCallback(
+    () => setTimer(startingTime),
+    [setTimer, startingTime]
+  )
+
+  return [timer, start, reset, stage] as const
+}
+
+export default useTimer
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,5 +1,6 @@
 import getText from "../adapters/getText"
 import GuidedText from "../components/GuidedText"
+import useTimer from "../hooks/useTimer"
 import useTypingText from "../hooks/useTypingText"
 
 export const getStaticProps = async () => {
@@ -11,7 +12,15 @@ export const getStaticProps = async () => {
 }
 
 const Test = ({ response }: { response: string }) => {
-  return <GuidedText text={response} />
+  const [timer, start, reset, stage] = useTimer()
+  return (
+    <>
+      <GuidedText text={response} />
+      <button onClick={start}>start</button>
+      <button onClick={reset}>reset</button>
+      {timer}
+    </>
+  )
 }
 
 export default Test
